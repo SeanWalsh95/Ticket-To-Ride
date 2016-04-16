4986a47c167751658fc6afd456796a3b2ef40e41
SeanWalsh95 <SeanWalsh95@users.noreply.github.com>
2016-04-16 13:55:13 -0400

    Dest Calss

--- Dest.java
+++ Dest.java
@@ -1,15 +1,35 @@
-
+import java.util.*;
 /**
- * Write a description of class Dest here.
+ * Write a description of class Destination here.
  * 
  * @author (your name) 
  * @version (a version number or a date)
  */
 public class Dest extends Card
 {
-    // instance variables - replace the example below with your own
-    protected CityName cityA;
-    protected CityName cityB;
-    protected String imagePath;
-    protected int pointValue;
+    CityName cityA, cityB;
+    private static final String path = "resources\\images\\cards\\destination cards\\";
+    public Dest(String line, String delim){
+        StringTokenizer st = new StringTokenizer(line,delim);
+        String cityAStr = st.nextToken();
+        String cityBStr = st.nextToken();
+        
+        //identifying message for this class
+        String classMsg = "(ERR CONST Destination)";
+        
+        try{
+            cityA = CityName.valueOf(cityAStr.replace(" ",""));
+        }catch(Exception e){
+            System.err.println(classMsg+cityAStr+": Invalid City name");
+        }
+        try{
+            cityB = CityName.valueOf(cityBStr.replace(" ",""));
+        }catch(Exception e){
+            System.err.println(classMsg+cityBStr+": Invalid City name");
+        }
+    }
+    
+    public String getImage(){
+        return path+cityA+"_"+cityB+".png";
+    }
 }
