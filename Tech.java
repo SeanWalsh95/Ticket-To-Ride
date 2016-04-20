981bb9d5b0e5dac124906a0b23cbd77f1ece1407
SeanWalsh95 <SeanWalsh95@users.noreply.github.com>
2016-04-20 12:24:53 -0400

    Update to Tech

--- Tech.java
+++ Tech.java
@@ -1,33 +1,61 @@
-
+import java.util.*;
+import java.io.File;
 /**
- * Write a description of class Tech here.
+ * Write a description of class Technologies here.
  * 
  * @author (your name) 
  * @version (a version number or a date)
  */
-public class Tech
+public class Tech extends Card
 {
     int numberAvalable, cost;
-    String id, name, description;
-    
-    /**
-     * Constructor for objects of class Tech
-     */
-    public Tech()
-    {
-        // initialise instance variables
-        int x = 0;
-    }
+    Technology name;
+    String description, nameString;
+    private static final String path = "resources\\images\\cards\\Technologies\\";
 
     /**
-     * An example of a method - replace this comment with your own
-     * 
-     * @param  y   a sample parameter for a method
-     * @return     the sum of x and y 
-     */
-    public int sampleMethod(int y)
-    {
-        // put your code here
-        return 1 + y;
+     * Constructor that takes a string deliminated with the given deliminator to initlize the values of this class
+     * in the following format (example uses ";" as a deliminator) name;cost;numberAvalable;description*/
+    public Tech(String line , String delim){
+        // the following block of code breaks a single string into the 
+        // parameter values for the class storing them in local strings to be
+        // parsed into values for the class
+        StringTokenizer st = new StringTokenizer(line,delim);
+        String nameStr = st.nextToken();
+        String costStr = st.nextToken();
+        String numberAvalableStr = st.nextToken();
+        String description = st.nextToken();
+
+        //identifying message for this class
+        String classMsg = "(ERR CONST Tech)";
+
+        //attempts to parse the values from the given string into the classes values
+        //if a parse fails it is caught and an error is printed
+        try{
+            name = Technology.valueOf(nameStr.replace(" ",""));
+            nameString = nameStr;
+        }catch(Exception e){
+            System.err.println(classMsg+nameStr+": Invalid Tech name");
+        }
+        try{
+            cost = Integer.parseInt(costStr);
+        }catch(Exception e){
+            System.err.println(classMsg+cost+": Cannot parse cost to int");
+        }
+        try{
+            numberAvalable = Integer.parseInt(numberAvalableStr);
+        }catch(Exception e){
+            System.err.println(classMsg+numberAvalable+": Cannot parse numberAvalable to int");
+        }
+    }
+
+    public String getImage(){
+        String filePath = path+name+".jpg";
+        try(Scanner sc = new Scanner(new File(filePath))){
+            return filePath;
+        }catch(Exception e){
+            System.err.println("(ERR Tech.getImage): Cannot find file \""+filePath+"\"");
+        }
+        return "";
     }
 }
---
+++ Technology.java
@@ -0,0 +1,26 @@
+
+/**
+ * Enumeration class Technology - write a description of the enum class here
+ * 
+ * @author (your name here)
+ * @version (version number or date here)
+ */
+public enum Technology
+{
+    WalesConcession,
+    IrelandFranceConcession,
+    ScotlandConcession,
+    MechanicalStoker,
+    SuperheatedSteamBoiler,
+    Propellers,
+    Booster,
+    BoilerLagging,
+    SteamTurbine,
+    DoubleHeading,
+    RightOfWay,
+    Thermocompressor,
+    WaterTenders,
+    RiskyContracts,
+    EqualisingBeam,
+    DieselPower
+}
