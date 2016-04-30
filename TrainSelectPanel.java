92043aea84d155d9dcbb7ab9e1aba3fc81cc8243
unknown <s15wals@S60807.siena.edu>
2016-04-29 20:06:47 -0400

    TrainSelect

---
+++ TrainSelectPanel.java
@@ -0,0 +1,124 @@
+import java.awt.event.*;
+import javax.swing.*;
+import java.util.*;
+import java.awt.*;
+/**
+ * Write a description of class TrainSelectPanel here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class TrainSelectPanel extends JPanel {
+
+    // backround image to be drawn behind everything
+    Image background;
+    // int's representing various static vlaues
+    int cardWidth = 147, cardHeight = 94, rows = 2;
+    // String to be printed at the top of the Panel
+    String title;
+
+    ArrayList<Card> selected, hand;
+
+    //order of printed cards
+    RouteColor[] order = new RouteColor[]{RouteColor.BLACK,
+            RouteColor.GREEN, RouteColor.BLUE, RouteColor.YELLOW,
+            RouteColor.NEUTRAL, RouteColor.ORANGE, RouteColor.RED,
+            RouteColor.WHITE, RouteColor.PINK};
+
+    /**
+     * Constructor for objects of class TrainSelectPanel
+     */
+    public TrainSelectPanel(String title, ArrayList<Card> playersHand)
+    {
+        JPanel self = this;
+        this.setLayout(null);
+        this.background = ImgLib.trainPickerBackground;
+        this.title = title;
+        this.hand = new ArrayList<Card>(playersHand);
+        this.selected = new ArrayList<Card>();
+
+        JButton purchaseButt = new JButton("Confirm");
+        purchaseButt.setBounds(543,828,174,51);
+        purchaseButt.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent e) {
+                ((JDialog) SwingUtilities.windowForComponent(self)).dispose();
+            }
+        });
+        this.add(purchaseButt);
+    }
+    
+    /**
+     * PaintComponent for this JPanel component
+     *
+     * @param g the Graphics object for this Class
+     */
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        g.drawImage(background, 0, 0, this);
+
+        // sets the curent font to a larger size
+        g.setFont(g.getFont().deriveFont(30F));
+        g.setColor(Color.WHITE);
+
+        // centers the title text
+        int titleBuffer = 
+            (this.getWidth()-(g.getFontMetrics().stringWidth(title))) / 2;
+
+        // paints the title text
+        g.drawString(title, titleBuffer, 50);
+
+        g.drawString("Your Hand", 225, 150);
+        g.drawString("Selected Cards", 875, 150);
+
+        g.setFont(g.getFont().deriveFont(20F));
+        drawCards(g,155,184,hand);
+        drawCards(g,801,184,selected);
+
+    }
+
+    public void drawCards(Graphics g,int leftBorder, int topBorder, ArrayList<Card> list){
+        JPanel self = this;
+
+        for (int i = 0; i < 9; i++) {
+            int x = ((i % rows) * 60) + (i % rows) * cardWidth + leftBorder;
+            int y = ((i / rows) * 10) + (i / rows) * cardHeight + topBorder;
+            int count = CardCounter.countTrainColor(list, order[i]);
+            if (count >= 0) {
+                g.drawImage(Train.getImage(order[i]), x, y, cardWidth,
+                    cardHeight, this);
+                if(list == hand)
+                    g.setColor(Color.WHITE);
+                else
+                    g.setColor(Color.RED);
+                g.drawString(count + "", x - 15, y + 25);
+                RouteColor rc = order[i];
+                JLabel lbl = new JLabel();
+                lbl.setBounds(x, y, cardWidth, cardHeight);
+                int index = i;
+                lbl.addMouseListener(new java.awt.event.MouseAdapter() {
+                        public void mousePressed(MouseEvent e) {
+                            if(list == hand){
+                                for(int i=0; i < hand.size(); i++){
+                                    Train t = (Train)hand.get(i);
+                                    if(t.color == order[index]){
+                                        selected.add(hand.remove(i));
+                                        self.repaint();
+                                    }
+                                }
+                            }else if(list == selected){
+                                for(int i=0; i < selected.size(); i++){
+                                    Train t = (Train)selected.get(i);
+                                    if(t.color == order[index]){
+                                        hand.add(selected.remove(i));
+                                        self.repaint();
+                                    }
+                                }
+                            }
+                        }
+                    });
+                this.add(lbl);
+
+            }
+        }
+    }
+}
