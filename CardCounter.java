85dfe83899245bb237d71f62c85431d66f19dcd5
nm25tunn <nm25tunn@siena.edu>
2016-05-02 20:24:32 -0400

    Update CardCounter.java

--- CardCounter.java
+++ CardCounter.java
@@ -7,6 +7,14 @@ import java.util.*;
  * @version 1.0
  */
 public class CardCounter {
+    /**
+     * counts cards of a specified color
+     * 
+     * @param list The ArrayList you want to count the color
+     * @param color The color you want to check the count of
+     * 
+     * @return The number of trains of that color in the list
+     */
     public static int countTrainColor(ArrayList<Card> list, RouteColor color) {
         int count = 0;
         for (Card c : list)
